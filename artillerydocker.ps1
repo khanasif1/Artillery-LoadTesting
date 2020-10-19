2c7687cbdec69a8c4998ed8c94b183fcbbe21dff
Asif Khan <askha@microsoft.com>
2020-10-19 22:13:44 +1100

    updates

--- artillerydocker.ps1
+++ artillerydocker.ps1
@@ -1,14 +1,32 @@
 docker images -a 
 docker ps -a
 
-docker rm artilleryloadtest -f
-docker rmi artillery-aci:latest -f
+
+#***********************
+#****Build Image********
+#***********************
 
 docker build -t artillery-aci:latest .
+
+
+#***********************
+#******RUN Image********
+#***********************
 
 docker run -d --name artilleryloadtest artillery-aci:latest
 
 
-docker run  -it artilleryloadtest
+#***********************
+#****Get Container Log**
+#***********************
 
 docker logs --details  artilleryloadtest
+
+
+#***********************
+#****Cleanup container**
+#***********************
+
+
+docker rm artilleryloadtest -f
+docker rmi artillery-aci:latest -f
